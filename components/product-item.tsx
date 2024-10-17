f17579ba4099177c298a7226a2bf780d2132622d
Eric Pereira <ericpereira234@gmail.com>
2024-10-17 07:51:58 -0300

    criando a home

--- components/product-item.tsx
+++ components/product-item.tsx
@@ -1,4 +1,5 @@
-import { Text } from "react-native";
+import { Text, Pressable, StyleSheet, Image, View } from "react-native";
+import { Link } from "expo-router";
 
 //arquivos internos
 import { Product } from "../types/product";
@@ -8,8 +9,36 @@ type Props = {
     data: Product;
 }
 
-export const ProductItem = ({data}:Props)=>{
+export const ProductItem = ({ data }: Props) => {
     return (
-        <Text>{data.title}</Text>
+        <Link href={`/product/${data.id}`} asChild>
+            <Pressable style={styles.component} >
+                <Image
+                    style={styles.img}
+                    source={{ uri: data.image }}
+                    resizeMode="cover"
+                />
+                <View style={styles.info} >
+                    <Text>{data.title}</Text>
+                    <Text>{data.description}</Text>
+                    <Text>{data.price.toFixed(2)}</Text>
+                </View>
+            </Pressable>
+        </Link>
     )
 }
+
+const styles = StyleSheet.create({
+    component: {
+        flexDirection: 'row',
+        marginBottom: 20
+    },
+    img: {
+        width: 100,
+        height: 100,
+        backgroundColor: '#ccc'
+    },
+    info: {
+        flex: 1
+    }
+})
